1e73ffd25430c5cf1daff5c6eda5955fad05349b
jdbaktay <85709425+jdbaktay@users.noreply.github.com>
2023-04-28 22:42:30 +1200

    Removed specific path.

--- calc_spectralfxn.py
+++ calc_spectralfxn.py
@@ -171,7 +171,7 @@ gamma = float(sys.argv[8])
 
 params = (model, x, y, z, D)
 
-path = '/Users/joshuabaktay/Desktop/local data/states'
+path = ''      # '/Users/joshuabaktay/Desktop/local data/states'
 
 filename = '%s_AL_%.2f_%.2f_%.2f_%03i_.txt' % params
 AL = np.loadtxt(os.path.join(path, filename), dtype=complex)
