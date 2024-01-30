Corrected calculation of OTM fixed point. Simplified calculation to only compute form factors.

calc_spectralfxn.py
@@ -1,16 +1,32 @@
 import numpy as np
+import ncon as nc
 import scipy.linalg as spla
 import scipy.sparse.linalg as spspla
-import matplotlib.pyplot as plt
-import ncon as nc
 import functools
-import os
 import sys
-import matplotlib.colors as colors
+import os
+
 from mps_tools import checks
 
-def lorentzian(x, x0, gamma):
-    return (1 / np.pi) * ((0.5 * gamma)/((x - x0)**2 + (0.5 * gamma)**2))
+def calc_expectation_val(o, AC, lfp):
+    tensors = [lfp, AC, o, AC.conj()]
+    indices = [(3, 4), (4, 2, 5), (1, 2), (3, 1, 5)]
+    return nc.ncon(tensors, indices)
+
+def my_corr_length(A, X0, tol):
+    def left_transfer_op(X):
+        tensors = [A, X.reshape(D, D), A.conj()]
+        indices = [(1, 2, -2), (3, 2), (1, 3, -1)]
+        contord = [2, 3, 1]
+        return nc.ncon(tensors,indices,contord).ravel()
+
+    E = spspla.LinearOperator((D * D, D * D), matvec=left_transfer_op)
+
+    # k must be LARGER THAN OR EQUAL TO 2
+    evals = spspla.eigs(E, k=4, which="LM", v0=X0, tol=tol, 
+                                return_eigenvectors=False
+                                )
+    return -1.0 / np.log(np.abs(evals[-2])), evals
 
 def op_transfer_matrix(A, B):
     def left_transfer_op(X):
@@ -26,46 +42,54 @@ def op_transfer_matrix(A, B):
         return nc.ncon(tensors,indices,contord).ravel()
 
     E = spspla.LinearOperator((D * D, D * D), matvec=left_transfer_op)
-    wl, lfp_AB = spspla.eigs(E, k=1, which='LM', tol=1e-14)
+    wl, lfp_AB = spspla.eigs(E, k=2, which='LM', tol=1e-14)
     print('wl', wl, np.abs(wl))
-
-    lfp_AB = lfp_AB.reshape(D, D)
+    print('phi', np.angle(wl))
+   
+    if np.angle(wl[0]) > 0:
+        lfp_AB = lfp_AB[:,0].reshape(D, D)
+        print(np.angle(wl[0]))
+        phi = np.angle(wl[0])
+    if np.angle(wl[0]) < 0:
+        lfp_AB = lfp_AB[:,1].reshape(D, D)
+        print(np.angle(wl[1]))
+        phi = np.angle(wl[1])         
 
     E = spspla.LinearOperator((D * D, D * D), matvec=right_transfer_op)
-    wr, rfp_AB = spspla.eigs(E, k=1, which='LM', tol=1e-14)
+    wr, rfp_AB = spspla.eigs(E, k=2, which='LM', tol=1e-14)
     print('wr', wr, np.abs(wr))
+    print('phi', np.angle(wr))
+    
+    if np.angle(wr[0]) > 0:
+        rfp_AB = rfp_AB[:,0].reshape(D, D)
+        print(np.angle(wr[0]))
+    if np.angle(wr[0]) < 0:
+        rfp_AB = rfp_AB[:,1].reshape(D, D)
+        print(np.angle(wr[1]))
 
-    rfp_AB = rfp_AB.reshape(D, D)
-
-    # Overlap check
     norm = np.trace(lfp_AB @ rfp_AB)
     print('(l|r)', norm)
 
-    # Normalize fixed points # (supposedly do not need to normalize)
-    # lfp_AB /= np.sqrt(norm)
-    # rfp_AB /= np.sqrt(norm)
-
-    # Projector check; comment out for large bond dim
-    # P_AB = nc.ncon([rfp_AB, lfp_AB], [(-1, -2), (-4, -3)]).reshape(D**2, D**2)
-    # print('P^2 - P', spla.norm((P_AB @ P_AB) - P_AB))
-
-    return lfp_AB
+    # Normalize fixed points
+    lfp_AB /= np.sqrt(norm)
+    rfp_AB /= np.sqrt(norm)
+    
+    print('(l|r)', np.trace(lfp_AB @ rfp_AB))
+    return lfp_AB, np.exp(1j * phi)
 
 def calc_specfxn(AL, AR, AC, 
-                 excit_energy, excit_states, 
-                 mom_vec, freq_vec, gamma, O):
+                 excit_energy, excit_states, mom_vec, 
+                 sp, sm):
 
+    lz, phase = op_transfer_matrix(AL, AL)
+    
     def left_env(X):
         X = X.reshape(D, D)
 
-        tensors = [X, AR, -sz, AR.conj()]
+        tensors = [X, AR, -sz, (phase * AR).conj()]
         indices = [(3, 4), (4, 2, -2), (1, 2), (3, 1, -1)]
         contord = [3, 4, 1, 2]
         XT = nc.ncon(tensors, indices, contord)
-
-        # We do not need the pseudo-inverse even for p=0 since the 
-        # leading eval of the operator transfer op is < 1
-
         return (X - np.exp(-1.0j * p) * XT).ravel()
 
     def right_env(X):
@@ -82,21 +106,18 @@ def calc_specfxn(AL, AR, AC,
         else:
             return (X - np.exp(+1.0j * p) * XT).ravel()
 
-    lz = op_transfer_matrix(AL, AL)
-
-    # As in the static case, we do not regularize ladder operators
-
-    specfxn = []
-    for i in range(mom_vec.size):
-        p = mom_vec[i]
-        print('p', p)
-
-        specfxn_p = np.zeros(freq_vec.size)
+    Ap, Ah = [], []
+    for i, p in enumerate(mom_vec):
+        if p / np.pi == 0:
+            sp -= calc_expectation_val(sp, AC, np.eye(D)) * np.eye(d)
+            sm -= calc_expectation_val(sm, AC, np.eye(D)) * np.eye(d)
+            print('p', p)
+   
         for j in range(excit_states.shape[2]):
             X = excit_states[i,:,j].reshape((d - 1) * D, D)
             B = np.tensordot(VL, X, axes=(2, 0))
 
-            tensors = [lz, B, -sz, AC.conj()]
+            tensors = [lz, B, -sz, (phase * AC).conj()]
             indices = [(3, 4), (4, 2, -2), (1, 2), (3, 1, -1)]
             contord = [3, 4, 1, 2]
             left_vec = nc.ncon(tensors, indices, contord)
@@ -115,6 +136,7 @@ def calc_specfxn(AL, AR, AC,
             right_env_op = spspla.LinearOperator((D * D, D * D), 
                                                  matvec=right_env
                                                  )
+
             LB = spspla.gmres(left_env_op, left_vec.ravel(),
                                            x0=rand_init.ravel(), 
                                            tol=1e-14, 
@@ -127,150 +149,120 @@ def calc_specfxn(AL, AR, AC,
                                             atol=1e-14
                                             )[0].reshape(D, D)
 
-            tensors = [lz, B, O, AC.conj()]
+            tensors = [lz, B, sp, AC.conj()]
             indices = [(3, 4), (4, 2, 5), (1, 2), (3, 1, 5)]
             contord = [3, 4, 5, 1, 2]
-            t1 = nc.ncon(tensors, indices, contord)
+            sp1 = nc.ncon(tensors, indices, contord)
 
-            tensors = [lz, AL, O, AL.conj(), RB]
+            tensors = [lz, AL, sp, AL.conj(), RB]
             indices = [(3, 4), (4, 2, 5), (1, 2), (3, 1, 6), (5, 6)]
             contord = [3, 4, 5, 6, 1, 2]
-            t2 = nc.ncon(tensors, indices, contord)
+            sp2 = nc.ncon(tensors, indices, contord)
 
-            tensors = [LB, AR, O, AR.conj()]
+            tensors = [LB, AR, sp, AR.conj()]
             indices = [(3, 4), (4, 2, 5), (1, 2), (3, 1, 5)]
             contord = [3, 4, 5, 1, 2]
-            t3 = nc.ncon(tensors, indices, contord) # This term is not zero anymore with lz (in LB)
+            sp3 = nc.ncon(tensors, indices, contord)
 
-            spec_weight = np.abs(t1 
-                               + np.exp(+1j * p) * t2
-                               + np.exp(-1j * p) * t3
-                               )
+            tensors = [lz, B, sm, AC.conj()]
+            indices = [(3, 4), (4, 2, 5), (1, 2), (3, 1, 5)]
+            contord = [3, 4, 5, 1, 2]
+            sm1 = nc.ncon(tensors, indices, contord)
 
-            lorentz_j = (2 * np.pi 
-                         * lorentzian(freq_vec, excit_energy[i,j], gamma)
-                         * spec_weight**2
-                         )
+            tensors = [lz, AL, sm, AL.conj(), RB]
+            indices = [(3, 4), (4, 2, 5), (1, 2), (3, 1, 6), (5, 6)]
+            contord = [3, 4, 5, 6, 1, 2]
+            sm2 = nc.ncon(tensors, indices, contord)
 
-            specfxn_p += lorentz_j
-        specfxn.append(specfxn_p)
-    return np.array(specfxn).reshape(mom_vec.size, freq_vec.size).T
+            tensors = [LB, AR, sm, AR.conj()]
+            indices = [(3, 4), (4, 2, 5), (1, 2), (3, 1, 5)]
+            contord = [3, 4, 5, 1, 2]
+            sm3 = nc.ncon(tensors, indices, contord)
 
-############ Load data #############
+            Ap.append(np.abs(sp1 
+                           + np.exp(+1j * p) * sp2 
+                           + np.exp(-1j * p) * sp3
+                           )
+                      )
+
+            Ah.append(np.abs(sm1
+                           + np.exp(+1j * p) * sm2
+                           + np.exp(-1j * p) * sm3
+                            )
+                      )
+
+    Ap = np.array(Ap).reshape(mom_vec.size, excit_states.shape[2])
+    Ah = np.array(Ah).reshape(mom_vec.size, excit_states.shape[2])
+    return Ap, Ah
+
+########################### Initialization #############################
 
 model = str(sys.argv[1])
-
 d = int(sys.argv[2])
 D = int(sys.argv[3])
-
-# params not needed
 x = float(sys.argv[4])
 y = float(sys.argv[5])
 z = float(sys.argv[6])
 g = float(sys.argv[7])
-
 N = int(sys.argv[8])
-gamma = float(sys.argv[9])
 
-params = (model, x, y, z, g, D)
+params = (model, x, y, z, g, D, N)
+print('input params', params)
 
-path = '' #'/Users/joshuabaktay/Desktop/local data/states/'
+path = '' 
 
-filename = '%s_AL_%.2f_%.2f_%.2f_%.2f_%03i_.txt' % params
-AL = np.loadtxt(os.path.join(path, filename), dtype=complex)
-AL = AL.reshape(d, D, D).transpose(1, 0, 2)
+filename = f'{model}_gs_{x}_{y}_{z}_{g}_{D:03}_.npz'
+gs_mps = np.load(os.path.join(path, filename))
 
-filename = '%s_AR_%.2f_%.2f_%.2f_%.2f_%03i_.txt' % params
-AR = np.loadtxt(os.path.join(path, filename), dtype=complex)
-AR = AR.reshape(d, D, D).transpose(1, 0, 2)
+# transpose needed for change to easier index conventions
+AL = gs_mps['AL'].transpose(1, 0, 2)
+AR = gs_mps['AR'].transpose(1, 0, 2)
+C = gs_mps['C']
 
-filename = '%s_C_%.2f_%.2f_%.2f_%.2f_%03i_.txt' % params
-C = np.loadtxt(os.path.join(path, filename), dtype=complex)
-C = C.reshape(D, D)
+path = ''
 
-filename = '%s_disp_%.2f_%.2f_%.2f_%.2f_%03i_%05i_.dat' % (*params, N)
-disp = np.loadtxt(os.path.join(path, filename))
-print(filename)
-mom_vec = disp[:, 0]
-excit_energy  = disp[:, 1:]
+filename = f'{model}_excits_{x}_{y}_{z}_{g}_{D:03}_{N:05}_.npz'
+excits = np.load(os.path.join(path, filename))
 
-print('excit_energy', excit_energy.shape)
-print('excit_energy min', excit_energy.min())
-print('excit_energy max', excit_energy.max())
-
-filename = '%s_estate_%.2f_%.2f_%.2f_%.2f_%03i_%05i_.dat' % (*params, N)
-excit_states = np.loadtxt(os.path.join(path, filename), dtype=complex)
-excit_states = excit_states.reshape(excit_energy.shape[0], 
-                                    (d - 1) * D**2, 
-                                    excit_energy.shape[1]
-                                    )
-
-print('excit_states', excit_states.shape)
-print(filename)
-
-checks(AL.transpose(1, 0, 2), AR.transpose(1, 0, 2), C)
+mom_vec = excits['mom']
+excit_energy = excits['evals']
+excit_states = excits['estates']
 
 if d == 2:
-    sx = np.array([[0, 1],[1, 0]]) # gets 1/2
-    sy = np.array([[0, -1j],[1j, 0]]) # gets 1/2
-    sz = np.array([[1, 0],[0, -1]]) # gets 1/2
+    sx = np.array([[0, 1],[1, 0]]) 
+    sy = np.array([[0, -1j],[1j, 0]]) 
+    sz = np.array([[1, 0],[0, -1]]) 
 
 if d == 3:
-    sx = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) # gets 1/sqrt2
-    sy = np.array([[0, -1j, 0], [1j, 0, -1j], [0, 1j, 0]]) # gets 1/sqrt2
+    sx = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) 
+    sy = np.array([[0, -1j, 0], [1j, 0, -1j], [0, 1j, 0]]) 
     sz = np.array([[1, 0, 0], [0, 0, 0], [0, 0, -1]])
 
 sp = 0.5 * (sx + 1.0j * sy)
 sm = 0.5 * (sx - 1.0j * sy)
 n = 0.5 * (sz + np.eye(d))
 
-############ Precompute steps ############
+###################### Compute spectral fxn ############################
 
 VL = spla.null_space(AL.conj().reshape(D * d, D).T)
 VL = VL.reshape(D, d, (d - 1) * D)
 
-freq_min = excit_energy.min() - (3 * gamma)
-freq_max = excit_energy.max() + (3 * gamma)
+Ap, Ah = calc_specfxn(AL, AR, np.tensordot(AL, C, axes=(2, 0)), 
+                      excit_energy, excit_states, mom_vec, 
+                      sp, sm
+                      )
 
-num = int(np.ceil(5 * ((freq_max - freq_min) / gamma)))
+print('Ap, Ah:', Ap.shape, Ah.shape)
 
-freq_vec = np.linspace(freq_min, freq_max, num)
+_, corrlens = my_corr_length(AL.transpose(1, 0, 2), C, 1e-14)
 
-print('gamma', gamma)
-print('freq vec size', freq_vec.size)
-print('delta omega', freq_vec[1] - freq_vec[0])
+path = ''
 
-specfxn = calc_specfxn(AL, AR, np.tensordot(AL, C, axes=(2, 0)), 
-                       excit_energy, excit_states, 
-                       mom_vec, freq_vec, gamma, sp
-                       )
-
-print(specfxn.shape)
-
-fig, ax = plt.subplots()
-A, B = np.meshgrid(mom_vec / np.pi, freq_vec)
-
-Z = specfxn
-
-print(Z.min(), Z.max())
-
-Z_min, Z_max = Z.min(), Z.max()
-Z = (Z - Z_min) / (Z_max - Z_min)
-
-print(Z.min(), Z.max())
-
-plot = ax.contourf(A, B, Z, levels=100)
-
-cbar = fig.colorbar(plot, format='%.3f')
-cbar.ax.set_ylabel('A(q, \u03C9)')
-ax.set_xlabel('q')
-ax.set_ylabel('\u03C9')
-plt.title('D='+str(D)+', gamma='+str(gamma)+', N='+str(N))
-plt.show()
-
-filename = '%s_Ap_%.2f_%.2f_%.2f_%.2f_%03i_%05i_%.2f_.txt' % (*params, N, gamma)
-np.savetxt(os.path.join(path, filename), np.column_stack((freq_vec, specfxn)))
-
-
-
+filename = f'{model}_specweights_{x}_{y}_{z}_{g}_{D:03}_{N:05}_.npz'
+np.savez(os.path.join(path, filename), mom=mom_vec, 
+                                       evals=excit_energy, 
+                                       Ap=Ap, 
+                                       Ah=Ah, 
+                                       corrlens=corrlens
+                                       )
 